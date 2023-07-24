f1fdac454ec1e1943d91216b55ea1ef20bda47bc
Nicholas Perry <nuperry2011@gmail.com>
2023-07-24 03:00:36 -0700

    update while

--- while_loops.py
+++ while_loops.py
@@ -1,1 +1,14 @@
 # User inputs and While loops!!!
+
+# parrot.py
+message = input("Tell me something, and I will repeat it back toy you: ")
+print(message)
+
+# Even_or_Odd.py
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+
+if number % 2 == 0:
+    print(f"The number {number} is even. ")
+else:
+    print(f"The number {number} is odd. ") 
