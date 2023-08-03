f468ce0b8073e19631a51aba6cd51692801c763f
Nicholas Perry <nuperry2011@gmail.com>
2023-08-03 01:30:38 -0700

    while update

--- while_loops.py
+++ while_loops.py
@@ -12,3 +12,43 @@ if number % 2 == 0:
     print(f"The number {number} is even. ")
 else:
     print(f"The number {number} is odd. ") 
+    
+    
+# Rental_Car.py
+car = input("What type of car are you looking for? ")
+print(f"{car.title()} that's an interesting car but I don't believe we have those, let's check to see if we have a Subaru.")
+
+# Restaurant_Seats.py
+name = input("Enter name: ")
+seats = input(f"Good Evening {name.title()}, how many people will you have dinning with you this evening? ")
+seats = int(seats)
+
+if seats > 8:
+    print(f"That will be a 10-15 min wait {name.title()} ")
+else:
+    print(f"Your table is now ready {name.title()}, please right this way ")
+    
+# Now let's practice while loops.
+# The for loop takes a collection of items and executes a block of code once for each item in the collection. In contras, the while loop runs as a long as or while, a certain condition is true
+# While loop in action: You can use a while loop to count up through a series of numbers. For example, the following while loop counts from 1-5
+# counting.py
+
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+    
+# Letting the User choose when to quit
+# This program will run as long as the user wants by putting most of the program inside a while loop. We'll define a quit value and then keep the program running as long as the user has not entered the quit value
+
+# parrot2.py
+
+prompt = "Tell me something, and I'll repeat it back to you: "
+prompt += " Enter 'quit' to end the program. "
+
+talk = ""
+while talk != 'quit':
+    talk = input(prompt)
+    print(talk)
+    
+    
