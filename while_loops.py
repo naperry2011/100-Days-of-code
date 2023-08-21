6ea0461acde703c634bf17231f5eb247bbb78c55
Nicholas Perry <nuperry2011@gmail.com>
2023-08-21 10:13:11 -0700

    update

--- while_loops.py
+++ while_loops.py
@@ -52,3 +52,15 @@ while talk != 'quit':
     print(talk)
     
     
+# Using a Flag
+# cities.py
+prompt = " \nPlease enter the name of a city you have visited:" 
+prompt += "\n(Enter 'quit' when you are finished.)"
+
+while True:
+    city = input(prompt)
+    
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
