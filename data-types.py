09255779dab7cedffe521ece43b9e98efc80fe46
Nicholas Perry <nuperry2011@gmail.com>
2024-06-12 01:59:14 -0700

    updating python git profile

---
+++ data-types.py
@@ -0,0 +1,17 @@
+#Strings
+greeting = 'Hello, friend!'
+# integers
+age = 35
+ages = 16
+#Floats
+speed1 = 4.5
+speed2 = 4.0
+speed3 = 4.
+
+
+#Boolean True/False or 1/0
+
+am_i_ugly = True
+are_you_ugly = False
+
+
