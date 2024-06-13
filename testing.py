ab2bfe7d5bf88cbfc8f98655ec1ff0156f5dd819
Nicholas Perry <nuperry2011@gmail.com>
2024-06-12 18:32:44 -0700

    school work

--- testing.py
+++ testing.py
@@ -14,3 +14,9 @@ tax_difference = tax_ripoffland - tax_lowtaxland
 
 # Printing the required output
 print(f"Your income is {income} and you would pay {tax_lowtaxland} income tax in Lowtaxland or {tax_ripoffland} income tax in Ripoffland. You would save {tax_difference} by paying taxes in Lowtaxland!")
+
+# input practice
+login = input("Enter your login: ")
+language = input("Enter your native language: ")
+
+print(f"Your username is {login} and your native language is {language}")
