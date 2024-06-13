ea330630b70ac1a5dea0dd512754e1e34080f2cb
Nicholas Perry <nuperry2011@gmail.com>
2024-06-12 22:59:53 -0700

    more content

--- testing.py
+++ testing.py
@@ -1,27 +1,33 @@
 # Given income
 income = 250000
 
-# Tax rates for Lowtaxland and Ripoffland
-lowtaxland_rate = 0.05
-ripoffland_rate = 0.43
+# # Tax rates for Lowtaxland and Ripoffland
+# lowtaxland_rate = 0.05
+# ripoffland_rate = 0.43
 
-# Calculating the tax amounts
-tax_lowtaxland = income * lowtaxland_rate
-tax_ripoffland = income * ripoffland_rate
+# # Calculating the tax amounts
+# tax_lowtaxland = income * lowtaxland_rate
+# tax_ripoffland = income * ripoffland_rate
 
-# Calculating the difference in tax amounts
-tax_difference = tax_ripoffland - tax_lowtaxland
+# # Calculating the difference in tax amounts
+# tax_difference = tax_ripoffland - tax_lowtaxland
 
-# Printing the required output
-print(f"Your income is {income} and you would pay {tax_lowtaxland} income tax in Lowtaxland or {tax_ripoffland} income tax in Ripoffland. You would save {tax_difference} by paying taxes in Lowtaxland!")
+# # Printing the required output
+# print(f"Your income is {income} and you would pay {tax_lowtaxland} income tax in Lowtaxland or {tax_ripoffland} income tax in Ripoffland. You would save {tax_difference} by paying taxes in Lowtaxland!")
 
-# input practice
-login = input("Enter your login: ")
-language = input("Enter your native language: ")
+# # input practice
+# login = input("Enter your login: ")
+# language = input("Enter your native language: ")
 
-print(f"Your username is {login} and your native language is {language}")
+# print(f"Your username is {login} and your native language is {language}")
 
 
-# Typecasting
-height_cm = float(input("Height converter: enter your height in cm: "))
-print("Your height in feet is :", height_cm / 30.48)
+# # Typecasting
+# height_cm = float(input("Height converter: enter your height in cm: "))
+# print("Your height in feet is :", height_cm / 30.48)
+
+# Salary calculator code
+hours = float(input("How many hours did you work last month? "))
+hourly_rate= float(input("What is your hourly rate? "))
+salary = hours * hourly_rate
+print(f"Last month, you earned ${salary} dollars")
